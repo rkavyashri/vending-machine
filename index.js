6158cf18720b754b9e73a17ad70bbde7449bf1ee
Kavyashri Raju <kavyashri.raju@aigilx.local>
2021-12-27 23:39:44 +0530

    functionality has been completed

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let http = require( 'http' );
-let fs = require( 'fs' );
 const Hapi = require( 'hapi' );
-const Path =require('path')
-const acceptedDenominations = [ 1,2,5,10,20,50,100,200,500,2000 ];
-//const Hapi = require('@hapi/hapi');
+const Path = require( 'path' );
+const acceptedDenominations = [ 10,20,50,100,200,500,2000 ];
+const itemsInVendingMachine = [
+    {
+         item: "lays",
+         price: 10
+    },
+    {
+      item: "kurkure",
+        price: 10
+    },
+    {
+        item: "pepsi",
+        price: 20
+    },
+    {
+        item: "sprite",
+        price: 20
+    },{
+        item: "dairymilk",
+        price: 10
+    }]
+
 const init = async () => {
     const server = new Hapi.Server( {
         host: 'localhost',
@@ -25,7 +43,7 @@ const init = async () => {
         method: 'GET',
         path: '/home',
         handler: (request,reply) => {
-            return reply.file ('./public/home.html')
+            return reply.file ('./public/views/home.html')
         }
     } )
     server.route( {
@@ -47,27 +65,40 @@ const init = async () => {
     } )
     server.route( {
         method: 'POST',
-        path: '/item/confirmation/{id}',
+        path: '/item/confirmation/{amount}/{item}',
         handler: ( request,reply ) => {
-            console.log( request.params.id )
-            let value=request.params.id
-            return reply.view('confirmation',{value:value})
+            console.log( request.params)
+            let amount = request.params.amount;
+            let item = request.params.item;
+            return reply.view('confirmation',{amount:amount,item:item})
            
         }
     } )
     server.route( {
         method: 'POST',
-        path: '/item/{id}',
+        path: '/deliver/{amount}/{item}',
         handler: ( request,reply ) => {
-            console.log(request.params.id)
-             return reply.file(`./public/images/${request.params.id}.jpg`)
+            let amountPaid = request.params.amount;
+            let item = request.params.item;
+           let itemPrice= itemsInVendingMachine.find(x => x.item === item)
+          //  let itemPrice = _.find( itemsInVendingMachine,{ 'item': item } )
+            if ( Number( amountPaid ) < itemPrice.price )
+            {
+                return `Item Price is - ${itemPrice.price} inserted amount is - ${amountPaid}. Try again with higher amount`;
+            } else
+            {
+                let bal = Number(amountPaid)-itemPrice.price ;
+            return reply.view( 'deliver',{ item:item,bal:bal })
+            }
+            
         }
     } )
     server.route( {
         method: 'POST',
-        path: '/cancel',
+        path: '/cancel/{amount}',
         handler: ( request,reply ) => {
-             return 'Take the refund amount '
+            let amountPaid = request.params.amount;
+             return 'Take the refund amount '+amountPaid
         }
     } )
     server.route( {
@@ -94,13 +125,18 @@ const init = async () => {
         
         }
     } )
- /*    server.route( {
-        method: 'POST',
-        path: '/item/{id}',
-        handler: (request,reply) => {
-            return `the item is ${request.params.id}`
+    server.route( {
+        method: 'GET',
+        path: '/deliver/images/{path*}',
+        handler:  {
+            directory: {
+                path: "./public/images",
+                    listing: false,
+                        index: false
+            }
+        
         }
-    } ) */
+    } )
     // starting the server
     const starter=async ()=> {
         await server.start();
@@ -116,83 +152,3 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
 });
 init();
-
-
-let itemsInVendingMachine = [ {
-    item: "Lays",
-    price: 10
-    },
-    {
-    item: "Kurkure",
-    price: 10
-    },
-    {
-        item: "Pepsi",
-        price: 22
-    },
-    {
-        item: "Sprite",
-        price: 22
-    },{
-        item: "dairyMilk",
-        price: 25
-    }]
-async function venderMachine () {
-    let input1 = process.argv[ 2 ];
-    let input2 = process.argv[ 3 ];
-    input2=parseInt(input2,10)
-    console.log( input1 )
-    console.log(input2)
-    if ( typeof input1 == 'string' )
-    {
-        input1=parseInt(input1,10)
-    }
-    
-    console.log('Welcome to Snacks Vending Machine')
-    console.log( 'Please enter the amount ' )
-    console.log( 'Entered amount is ',input1 )
-    if ( !input1 ||  !acceptedDenominations.includes(input1))
-    {
-        console.log( 'Sorry. Please enter a valid denomination' )
-        console.log('Valid denominations are ',acceptedDenominations)
-    } else
-    {
-        console.log('Please select the items for the amount ')
-        console.log( 'Please select the product of your choice. Press 1 for Lays, 2 for dairyMilk Silk, 3 for pepsi, 4 for kurkure, 5 for sprite ' )
-        switch ( input2 )
-        {
-            case 1:
-                console.log("Lays")
-                return "Lays";
-            case 2:
-                console.log( "DairyMilk" )
-                return "DairyMilk";
-            case 3:
-                    console.log( "Pepsi" )
-                return "Pepsi";
-            case 4:
-                    console.log( "Kurkure" )
-                return "Kurkure";
-            case 5:
-                    console.log( "Sprite" )
-                return "Sprite";
-        }
-        
-    }
-    
-}
-async function filterItemsByPrice (inputAmount,items) {
-    try {
-    } catch (err) {
-        throw err;
-    }
-}
-   /*  var server=http.createServer( function ( req,res ) {
-        res.writeHead( 200,{ 'Content-Type': 'text/html' } );
-        var readStream = fs.createReadStream( __dirname + '/index.html','utf8' );
-        readStream.pipe( res );
-    } )
-server.listen( 1000,'127.0.0.1' ) */
-
-
-//venderMachine()
